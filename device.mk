d01551c3aa796f42c0a004cf2e009637e4234645
Akash Srivastava <akashniki@gmail.com>
2023-11-04 11:47:34 +0530

    ginkgo: Build some missing LDAC libs * Bluetooth libs look for them
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- device.mk
+++ device.mk
@@ -74,6 +74,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
+    libldacBT_enc \
+    libldacBT_abr \
+    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
